8ef7ae6cc0aebd8ca63c3e03e590d0323925295a
Юшков Андрей Александрович <andrey.yushkov@gazprombank.ru>
2021-11-18 01:03:19 +0300

    add postgres db update .gitignore

--- src/main/java/com/boljshoj/thegame/app/ThegameApplication.java
+++ src/main/java/com/boljshoj/thegame/app/ThegameApplication.java
@@ -1,4 +1,4 @@
-package com.boljshoj.thegame;
+package com.boljshoj.thegame.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
